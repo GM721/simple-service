8614fba846d729c21064340cdfe25b71bb8ea842
GMIRA <gmiran2000@gmail.com>
2025-04-28 14:20:38 +0300

    Add GetTask

--- internal/service/entity.go
+++ internal/service/entity.go
@@ -5,3 +5,12 @@ type TaskRequest struct {
 	Title       string `json:"title" validate:"required"`
 	Description string `json:"description"`
 }
+
+type TaskResponse struct {
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	Created     string `json:"created_at"`
+	Updated     string `json:"updated_at"`
+}
